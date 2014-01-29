c3548d452d17dbd4d640d0c07b5fd87816ade32d
ddiggins <devynn.diggins@students.olin.edu>
2014-01-28 23:35:49 -0500

    Added .py files for hw2

---
+++ hw2/compare.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 28 23:00:23 2014
+
+@author: ddiggins
+"""
+
+def compare(x,y):
+    if x > y:
+        return 1
+    elif x == y:
+        return 0
+    else:
+        return -1
+
